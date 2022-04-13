97c722015cff78d24298dc5f37a8412fe29f66d6
Jennice <jennicelodi@gmail.com>
2022-04-13 11:25:42 -0700

    initial landing page

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,18 @@
 import React from "react"
-import PropTypes from "prop-types"
 import {
   BrowserRouter as  Router,
   Route,
   Switch
 } from 'react-router-dom'
 
+import Home from './pages/Home'
+
 class App extends React.Component {
   render () {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <>
+            <Home />
+      </>
     );
   }
 }
---
+++ app/javascript/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return(
+    <>
+      <h3>Hello, welcome to the apartment app</h3>
+      <p>This is where you can browse for available apartments</p>
+    </>
+    )
+  }
+}
+
+export default Home
