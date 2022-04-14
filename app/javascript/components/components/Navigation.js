de660e0aaa31c3b7b047c49e17e1574b49e54647
Jennice <jennicelodi@gmail.com>
2022-04-13 18:18:34 -0700

    added login and logout functionality, a text to indicate you are logged in

--- app/javascript/components/components/Navigation.js
+++ app/javascript/components/components/Navigation.js
@@ -12,41 +12,75 @@ import { NavLink } from 'react-router-dom'
 
 class Navigation extends Component {
   render() {
-    return(
-      <>
-        <div>
-            <Navbar
-                color="light"
-                expand="md"
-                light
-            >
-                <NavbarBrand to="/">
-                Apartment Finder
-                </NavbarBrand>
-                <NavbarToggler onClick={function noRefCheck(){}} />
-                <Collapse navbar>
-                    <Nav
-                        className="me-auto"
-                        navbar
-                    >
-                        <NavItem>
-                            <NavLink className="nav-link" to="/">
-                                Home
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" to="/test">
-                                Sign Up
-                            </NavLink>
-                        </NavItem>
-                    </Nav>
-                    <NavbarText>
-                    </NavbarText>
-                </Collapse>
-            </Navbar>
-        </div>
-      </>
-    )
+    const {
+        logged_in,
+        current_user,
+        new_user_route,
+        sign_in_route,
+        sign_out_route
+      } = this.props
+      console.log("logged_in:", logged_in)
+      console.log("current_user:", current_user)
+        return(
+        <>
+            <div>
+                <Navbar
+                    color="light"
+                    expand="md"
+                    light
+                >
+                    <NavbarBrand to="/">
+                    Apartment Finder
+                    </NavbarBrand>
+                    <NavbarToggler onClick={function noRefCheck(){}} />
+                    <Collapse navbar>
+                        <Nav
+                            className="me-auto"
+                            navbar
+                        >
+                            <NavItem>
+                                <NavLink className="nav-link" to="/">
+                                    Home
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/test">
+                                    Browse Apartments
+                                </NavLink>
+                            </NavItem>
+                            {logged_in &&
+                                <NavItem>
+                                    <a href={sign_out_route} className="nav-link">
+                                        Sign Out
+                                    </a>
+                                </NavItem>
+                            } 
+                            {!logged_in &&
+                                <NavItem>
+                                    <a href={sign_in_route} className="nav-link">
+                                        Sign In
+                                    </a>
+                                </NavItem>
+                            }
+                            {!logged_in &&
+                                <NavItem>
+                                    <a href={new_user_route} className="nav-link">
+                                        Sign Up
+                                    </a>
+                                </NavItem>
+                            }
+                        </Nav>
+                        <NavbarText>
+                            {logged_in && 
+                                <div>
+                                    Logged in as {current_user.email}
+                                </div> }
+                        </NavbarText>
+                    </Collapse>
+                </Navbar>
+            </div>
+        </>
+        )
   }
 }
 export default Navigation
